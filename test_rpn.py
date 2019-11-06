ed02ad268b1d2fe8388b021bc495c22cfb35e85f
17wegienkaj <jamesjw@umich.edu>
2019-11-06 14:13:07 -0500

    Added Test Functions for Mult, Div, and Exp

--- test_rpn.py
+++ test_rpn.py
@@ -8,6 +8,15 @@ class TestBasics(unittest.TestCase):
     def test_sub(self):
         result = rpn.calculate("5 3 -")
         self.assertEqual(2, result)
+    def test_mult(self):
+        result = rpn.calculate("5 3 *")
+        self.assertEqual(15, result)
+    def test_div(self):
+        result = rpn.calculate("6 3 /")
+        self.assertEqual(2, result)
+    def test_exp(self):
+        result = rpn.calculate("2 3 ^")
+        self.assertEqual(8, result)
     def test_badInput(self):
         with self. assertRaises(TypeError):
             rpn.calculate('1 2 3 +')
